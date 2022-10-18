Fix bug in rotate_batch_to_local_frame (rotation applied to every component of the position array now)

src/data_utils/Support.py
@@ -1,3 +1,4 @@
+import matplotlib.pyplot as plt
 import numpy as np
 import pylab as pl
 import math
@@ -94,20 +95,26 @@ def rotate_batch(batch_y, batch_x):
 def rotate_batch_to_local_frame(batch_y, batch_x):
   """
     inputs:
-      batch_y: trainning velocities batch containing vx, vy on the global frame
-      batch_x: trainning or prediction state batch containing x,y, vx, vy on the global frame
+      batch_y: training velocities batch containing vx, vy on the global frame
+      batch_x: training or prediction state batch containing x,y, vx, vy on the global frame
     """
   # rotate initial vx and vy in global frame to quary-agent local frame
   bx = batch_x.copy()  # rotated input batch values
   by = batch_y.copy()  # rotated output batch values
+
   for batch_idx in range(batch_x.shape[0]):
     for tbp_step in range(batch_x.shape[1]):
       heading = math.atan2(bx[batch_idx, tbp_step, 3], bx[batch_idx, tbp_step, 2])
+
       rot_mat = np.array([[np.cos(-heading), -np.sin(-heading)], [np.sin(-heading), np.cos(-heading)]])
-      bx[batch_idx, tbp_step, 2:] = np.dot(rot_mat, bx[batch_idx, tbp_step, 2:])
-      for pred_step in range(int(by.shape[2] / 2)):
-        by[batch_idx, tbp_step, 2 * pred_step:2 * pred_step + 2] = np.dot(rot_mat, by[batch_idx, tbp_step,
-                                                                                   2 * pred_step:2 * pred_step + 2])
+      for pred_step in range(bx.shape[2] // 2):
+        bx[batch_idx, tbp_step, 2 * pred_step:2 * (pred_step + 1)] = np.dot(
+          rot_mat, bx[batch_idx, tbp_step, 2 * pred_step:2 * (pred_step + 1)]
+        )
+      for pred_step in range(by.shape[2] // 2):
+        by[batch_idx, tbp_step, 2 * pred_step:2 * (pred_step + 1)] = np.dot(
+          rot_mat, by[batch_idx, tbp_step, 2 * pred_step:2 * (pred_step + 1)]
+        )
   return bx , by
 
 def plot_grid(ax, center, grid, grid_resolution, submap_size):